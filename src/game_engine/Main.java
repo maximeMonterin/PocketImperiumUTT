e7139851c2519594fc52f93b42f6bc64b0fde357
MaximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-01-02 20:09:37 +0100

    jsais plus mais tkt

--- src/game_engine/Main.java
+++ src/game_engine/Main.java
@@ -9,18 +9,21 @@ public class Main {
 
 		/* -_-_-_-_-_-_-_- INSTANCES -_-_-_-_-_-_-_- */
 
-		//Secteur 1
+					 /* PREMIERE RANGEE */
+		
+		//Secteur gauche
 		int[][] sectorBuilderOne = {{1,2},{3,7,4},{8,5}};
 		Sector sectorLeft = new Sector(sectorBuilderOne);
 
-		
-		//Secteur 2
+		//Secteur centre
 		int[][] sectorBuilderTwo = {{2,2},{10,7,4},{5,5}};
 		Sector sectorMiddle = new Sector(sectorBuilderTwo);
 
-		//Secteur 3
+		//Secteur droite
 		int[][] sectorBuilderThree = {{2,2},{10,7,4},{5,5}};
 		Sector sectorRight = new Sector(sectorBuilderThree);
+
+					 /* DEUXIEME RANGEE */
 
 		//Gameboard
 		Gameboard gameboard = Gameboard.getInstance();
