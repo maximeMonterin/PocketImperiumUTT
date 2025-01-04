f8b1caf6fcafe10d4cb5247df58c3e3f1d42bd2c
maximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-01-04 18:53:16 +0100

    instance liste sector dans gameboard

--- src/board/Gameboard.java
+++ src/board/Gameboard.java
@@ -5,55 +5,82 @@ import java.util.List;
 
 public class Gameboard {
 
-	private List<Sector> sectorList = new ArrayList<Sector>();
+	private List<List<Sector>> sectorList = new ArrayList<List<Sector>>();
+	private static List<Sector> tempList = new ArrayList<Sector>();
 	private String skin = "";
 	private static final Gameboard INSTANCE = new Gameboard();
-	
+		
 	private Gameboard() {}
-	
+		
 	public static Gameboard getInstance() {
 		return INSTANCE;
 	}
+
+	public void init(List<Sector> test){
+		Gameboard.tempList = test;
+
+		int sectorNumber = 9;
+		List<Sector> temp = new ArrayList<Sector>();
 	
-	
-	/* -_-_-_-_-_-_-_- METHODS -_-_-_-_-_-_-_- */
-	
-	public void addSectorInList(Sector sector) {
-		this.sectorList.add(sector);
-	}
-
-	public void skinGameboardBuilder() {
-
-		int skinKey = 1;
-		
-		//On crée une boucle while qui permet de parcourir dans l'entiereté les maps des skins des carrés (ayant des id allant de 1 à 5)
-		while(skinKey <= 13) {
-
-			for(int i = 0; i < this.getSectorList().size(); ++i) {
-
-				this.skin += this.getSectorList().get(i).getSectorSkin().get(skinKey);
-
+		for(int i = 0; i < sectorNumber; ++i){
+				
+			if(i == 3 || i == 6){
+				this.getSectorList().add(temp);
+				temp = new ArrayList<Sector>();
 			}
 
-			++skinKey;
-			this.skin += '\n';
+			temp.add(tempList.get(i));
+	
+			if(i == sectorNumber - 1){
+				this.getSectorList().add(temp);
+			}
 		}
-			
 	}
-	
-	
+			
+			
+			/* -_-_-_-_-_-_-_- METHODS -_-_-_-_-_-_-_- */
+		
+			/* 
+			public void skinGameboardBuilder() {
+		
+				int skinKey = 1;
+				
+				//On crée une boucle while qui permet de parcourir dans l'entiereté les maps des skins des carrés (ayant des id allant de 1 à 5)
+				while(skinKey <= 13) {
+		
+					for(int i = 0; i < this.getSectorList().size(); ++i) {
+		
+						this.skin += this.getSectorList().get(i).getSectorSkin().get(skinKey);
+		
+					}
+		
+					++skinKey;
+					this.skin += '\n';
+				}
+					
+			}*/
+			
+			
 	/* -_-_-_-_-_-_-_- GETTERS & SETTERS -_-_-_-_-_-_-_- */
-
-	public List<Sector> getSectorList() {
+		
+	public List<List<Sector>> getSectorList() {
 		return sectorList;
 	}
-
-	public void setSectorList(List<Sector> sectorList) {
+		
+	public void setSectorList(List<List<Sector>> sectorList) {
 		this.sectorList = sectorList;
+	}
+		
+	public static void setTempList(List<Sector> test){
+		tempList = test;
 	}
 
 	public String getGameboardSkin(){
 		return this.skin;
+	}
+
+	public String text(){
+		return this.getSectorList().toString();
 	}
 	
 	
--- src/game_engine/Main.java
+++ src/game_engine/Main.java
@@ -1,5 +1,8 @@
 package game_engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import board.Gameboard;
 import board.Sector;
 
@@ -25,17 +28,55 @@ public class Main {
 
 					 /* DEUXIEME RANGEE */
 
+		//Secteur gauche
+		int[][] sectorBuilderFour = {{1,2},{3,7,4},{8,5}};
+		Sector sectorFour = new Sector(sectorBuilderFour);
+		
+		//Secteur centre
+		int[][] sectorBuilderFive = {{2,2},{10,7,4},{5,5}};
+		Sector sectorFive = new Sector(sectorBuilderFive);
+		
+		//Secteur droite
+		int[][] sectorBuilderSix = {{2,2},{10,7,4},{5,5}};
+		Sector sectorSix = new Sector(sectorBuilderSix);
+
+					/* TROISIEME RANGEE */
+
+		//Secteur gauche
+		int[][] sectorBuilderSeven = {{1,2},{3,7,4},{8,5}};
+		Sector sectorSeven = new Sector(sectorBuilderSeven);
+		
+		//Secteur centre
+		int[][] sectorBuilderEight = {{2,2},{10,7,4},{5,5}};
+		Sector sectorEight = new Sector(sectorBuilderEight);
+		
+		//Secteur droite
+		int[][] sectorBuilderNine = {{2,2},{10,7,4},{5,5}};
+		Sector sectorNine = new Sector(sectorBuilderNine);
+
 		//Gameboard
+		List<Sector> temp = new ArrayList<Sector>();
+		temp.add(sectorRight);
+		temp.add(sectorMiddle);
+		temp.add(sectorLeft);
+
+		temp.add(sectorFour);
+		temp.add(sectorFive);
+		temp.add(sectorSix);
+
+		temp.add(sectorSeven);
+		temp.add(sectorEight);
+		temp.add(sectorNine);
+
+
 		Gameboard gameboard = Gameboard.getInstance();
-		gameboard.addSectorInList(sectorLeft);
-		gameboard.addSectorInList(sectorMiddle);
-		gameboard.addSectorInList(sectorRight);
+		gameboard.init(temp);
 
 
 		/* -_-_-_-_-_-_-_- TESTS -_-_-_-_-_-_-_- */
 
-		gameboard.skinGameboardBuilder();
-		System.out.println(gameboard.getGameboardSkin());
+		//gameboard.skinGameboardBuilder();
+		//System.out.println('\r' + gameboard.getGameboardSkin());
 
 	}
 		
